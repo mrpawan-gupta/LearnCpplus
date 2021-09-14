e1cdea9b8ab463d088d7d63a4cd2ddd07fb6e08d
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-14 22:46:08 +0530

    Hello World Program

---
+++ Core Cpp/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream> // Header file
+using namespace std;
+
+int main(int argc, char const *argv[]){   // Main Function
+
+    cout << "Hello World!!" << endl;   // Printing Statement
+    
+    return 0;
+}
