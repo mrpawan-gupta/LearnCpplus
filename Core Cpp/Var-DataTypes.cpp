55fd397bbf731b1309a6d286db4870c6974f7e69
Pawan Gupta <guptapawanro2017@gmail.com>
2021-09-14 22:56:29 +0530

    Varable And DataType

---
+++ Core Cpp/Var-DataTypes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    int i = 20 ;  // Integer Number DataTypes
+
+    float f = 10.16f ; // Float Number DataTypes
+
+    long l = 20000L;  // Long Number DataTypes
+
+    double d = 20.8 ; // Double Number DataTypes
+
+    long long  ll = 14684683441186561 ; // Long Number DataTypes
+
+    char c = 'X' ; // Character Data Type
+
+    cout << "The Given Values are : -" << endl;
+    cout << i << " " << f << " " << l << " " << d << " " << ll  << " " << c << endl;
+    
+    return 0;
+}
