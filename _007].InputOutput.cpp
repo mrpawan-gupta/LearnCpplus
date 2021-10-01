2317eefd31bdffb73b3960469d4558ce371e5eb5
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-01 19:17:11 +0530

    Input Output

---
+++ _007].InputOutput.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "Enter a Number : ";
+    int num ;
+    cin >> num; // user input statement
+
+    cout << "The number is : " << num << endl; // output statement
+
+    return 0;
+}
