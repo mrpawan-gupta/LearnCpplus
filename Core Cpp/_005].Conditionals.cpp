34480b162680b316335e9a75ae5eea67b760f2ac
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-01 19:15:03 +0530

    Conditional Statements

---
+++ Core Cpp/_005].Conditionals.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a = 10 , b = 2 ;
+
+    cout << "Numbers are : " << a << " & " << b << endl;
+
+    if(a>b) cout  << a << " is greater than " << b << endl;
+
+    else if(a == b) cout  << a << " is equal to " << b << endl;
+
+    else cout  << a << " is less than " << b << endl ;
+
+    return 0;
+}
