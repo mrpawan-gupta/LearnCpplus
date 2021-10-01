990d0661ca0c733d5b42a51abe1d50abe6dba506
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-01 20:15:43 +0530

    Create Pointer

--- 02]. Pointer -Array/_001].CreatePointer.cpp
+++ 02]. Pointer -Array/_001].CreatePointer.cpp
@@ -6,5 +6,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int a = 10005 ;
+
+    int* p = &a ; // pointer declaration
+
+    cout << " value of a = " << a << endl;
+    
+    cout << " value of a = " << *p << endl; // pointer derefrencing
     return 0;
 }
