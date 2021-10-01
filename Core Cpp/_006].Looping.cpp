760b6860c6124a00ac382946ddaca862dd94d1b5
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-01 19:10:40 +0530

    Loop

---
+++ Core Cpp/_006].Looping.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "Enter the last number  : ";
+    int num ;
+    cin >> num ;
+
+    cout << "The  series is : " << endl ;
+
+    for (int i = 0 ; i <= num ; i++) {
+        cout << i << " " ;
+    }
+    
+
+    return 0;
+}
