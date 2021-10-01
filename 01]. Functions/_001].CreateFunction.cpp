5a57f1c03c3479e14a59456cf1bcf30f9df9e491
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-01 19:53:42 +0530

    Create Function

--- 01]. Functions/_001].CreateFunction.cpp
+++ 01]. Functions/_001].CreateFunction.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//function for adition of two numbers ;
+
+
+int sum (int value1 , int value2){
+    // function declaration 
+    return value1 + value2;
+}
+
+// main function
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+
+    int a = 10 ;
+    int b = 10 ;
+
+    // function calling
+
+    cout << a << " + " << b << " = " << sum(a, b) << endl;
     return 0;
 }
