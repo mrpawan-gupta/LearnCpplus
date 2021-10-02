9fec9711a81f4aa2ed367b9632fcc598e2ac0574
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-02 11:07:46 +0530

    Traverse Array : Iterator

---
+++ 02]. Pointer -Array/_006].ArrayTraverse.cpp
@@ -0,0 +1,21 @@
+
+// array traversing using itrators
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int arr[5]={10, 0, 20, 0, 30}; //creating and initializing array 
+
+    //traversing array    
+    for (int i: arr){      // for each loop 
+
+        // int i --> iterator for every element of array 
+        
+        cout<<i<<"\n";    
+    }    
+    return 0;
+}
