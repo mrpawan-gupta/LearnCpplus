db9c2555b506d18ff3a70c2ecc677dc1149f6cfa
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-02 23:40:03 +0530

    Auto Storage Class

---
+++ 01]. Functions/_007].AutoStorageClass.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    auto y = 356; 
+
+    cout << "value : " << y << " of type " << typeid(y).name() << endl;
+    return 0;
+}
