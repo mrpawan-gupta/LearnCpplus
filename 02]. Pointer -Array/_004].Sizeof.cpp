ad2a7aa16ca067c373601d93f55d229cc172ae0d
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-02 11:00:17 +0530

    Size of : Operator

---
+++ 02]. Pointer -Array/_004].Sizeof.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a = 10002 ;
+    char c = 'A';
+    double d = 1054102.2 ;
+
+    cout << " int : " << a << " of size : " << sizeof(a) << endl;
+    cout << " double : " << d << " of size : " << sizeof(d) << endl;
+    cout << " char : " << c << " of size : " << sizeof(c) << endl;
+    return 0;
+}
