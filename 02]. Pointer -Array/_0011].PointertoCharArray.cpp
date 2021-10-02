c63dfca294f404eef567d1eeeb7d774f404e7637
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-02 23:53:30 +0530

    Pointer to String Array

---
+++ 02]. Pointer -Array/_0011].PointertoCharArray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+     char* names[5] = {"john",  
+                    "Peter",  
+                    "Marco",  
+                    "Devin",  
+                    "Ronan"};  
+    for(int i=0;i<5;i++)  
+    {  
+        cout << names[i] << endl;  
+    }  
+
+    return 0; 
+}
