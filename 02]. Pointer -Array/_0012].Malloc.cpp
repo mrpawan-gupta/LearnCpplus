7d8452c24889f85c6034f8183891471c804ffb3d
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-03 10:36:56 +0530

    Dynamic Memory : Malloc

---
+++ 02]. Pointer -Array/_0012].Malloc.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,i,*ptr,sum=0;    
+    printf("Enter number of elements: ");    
+    scanf("%d",&n);    
+    ptr=(int*)malloc(n*sizeof(int));  //memory allocated using malloc    
+    if(ptr==NULL)                         
+    {    
+        printf("Sorry! unable to allocate memory");    
+        exit(0);    
+    }    
+    printf("Enter elements of array: ");    
+    for(i=0;i<n;++i)    
+    {    
+        scanf("%d",ptr+i);    
+        sum+=*(ptr+i);    
+    }    
+    printf("Sum=%d",sum);    
+    free(ptr);  
+    
+    return 0;
+}
